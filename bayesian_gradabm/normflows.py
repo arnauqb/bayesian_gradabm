fb88274054681e2b8c4905f7d92c0210b5c50172
arnauq <arnau.quera@gmail.com>
2022-11-22 10:46:55 +0000

    reverting

--- bayesian_gradabm/normflows.py
+++ bayesian_gradabm/normflows.py
@@ -8,6 +8,8 @@ from tqdm import tqdm
 import os
 import matplotlib.pyplot as plt
 from copy import deepcopy
+import torch.distributed as dist
+import torch.multiprocessing as mp
 
 from .base import InferenceEngine
 from .utils import set_attribute
@@ -19,6 +21,17 @@ class NormFlows(InferenceEngine):
     @classmethod
     def read_parameters_to_fit(cls, params):
         return params["parameters_to_fit"]
+
+    def _make_dirs(self):
+        self.posteriors_path = self.results_path / "posteriors"
+        self.fits_path = self.results_path / "fits"
+
+        if self.posteriors_path.exists():
+            shutil.rmtree(self.posteriors_path)
+        self.posteriors_path.mkdir(parents=True)
+        if self.fits_path.exists():
+            shutil.rmtree(self.fits_path)
+        self.fits_path.mkdir(parents=True)
 
     def _setup_flow(self):
         flow_config = deepcopy(self.training_configuration["flow"])
@@ -34,8 +47,7 @@ class NormFlows(InferenceEngine):
         self.nfm = self.nfm.to(self.device)
 
     def _get_optimizer_and_scheduler(self):
-        return torch.optim.Adam(self.nfm.parameters(), lr=1e-4)
-        config = self.training_configuration["optimizer"]
+        config = deepcopy(self.training_configuration["optimizer"])
         optimizer_type = config.pop("type")
         if "milestones" in config:
             milestones = config.pop("milestones")
@@ -53,8 +65,7 @@ class NormFlows(InferenceEngine):
         return optimizer, scheduler
 
     def _get_loss(self):
-        return torch.nn.MSELoss(reduction="mean")
-        config = self.training_configuration["loss"]
+        config = deepcopy(self.training_configuration["loss"])
         loss_class = getattr(torch.nn, config.pop("type"))
         loss = loss_class(**config)
         return loss
@@ -66,7 +77,7 @@ class NormFlows(InferenceEngine):
         samples = np.where(~np.isinf(samples), samples, 0)
         samples = np.where(np.abs(samples) < 5, samples, 0)
         labels = [name.split(".")[-2] for name in self.priors]
-        true_values = [0.75, 1.25]
+        true_values = [1.5, 2.0]
         f = corner.corner(
             samples,
             labels=labels,
@@ -116,7 +127,7 @@ class NormFlows(InferenceEngine):
         f.savefig(self.results_path / "loss.png", dpi=150)
         return
 
-    def get_score(self, i, params, loss_fn, n_samples):
+    def _get_score(self, params, loss_fn, n_samples):
         params = torch.clip(params, min=-5, max=5)
         for i, name in enumerate(self.priors):
             set_attribute(self.runner, name, params[i].to(self.device))
@@ -131,76 +142,78 @@ class NormFlows(InferenceEngine):
         loss_i.backward()
         return loss_i
 
-
     def _get_forecast_score(self, loss_fn, n_samples=5):
-        loss = 0
-        if mpi_rank == 0:
-            params_list = [self.nfm.sample()[0][0] for i in range(n_samples)]
-        else:
-            params_list = None
-        mpi_comm.Barrier()
-        params_list = mpi_comm.bcast(params_list, root=0)
-        cases = None
+        # mpi_comm.Barrier()
+        # params_list = mpi_comm.bcast(params_list, root=0)
+        # params_list, _ = self.nfm.sample(n_samples)
         for i in range(n_samples):
             if i % mpi_size == mpi_rank:
-                loss_i = self.get_score(i, params_list[i], loss_fn, n_samples)
+                params = params_list[i]
+                loss_i = self._get_score(params, loss_fn, n_samples)
                 loss += loss_i
-        #mpi_comm.Barrier()
-        #losses = mpi_comm.gather(loss, root=0)
-        #if mpi_rank == 0:
+        # mpi_comm.Barrier()
+        # losses = mpi_comm.gather(loss, root=0)
+        # if mpi_rank == 0:
         #    losses = [loss.to(self.device) for loss in losses]
         #    losses = torch.hstack(losses)
         #    loss = torch.sum(losses)
-        #    return loss 
-        #else:
+        #    return loss
+        # else:
         #    return None
         return loss
 
-    def run(self):
-        loss_fn = self._get_loss()
+    def compute_score(self, rank, size, loss_fn, n_samples):
+        loss = 0
+        params_to_run = []
+        for i in range(n_samples):
+            dest_rank = i % size
+            if rank == 0:
+                params, _ = self.nfm.sample()
+                params = params[0].to("cpu")
+                params = torch.tensor([1.,1.])
+                print(params)
+                params = dist.send(tensor=params, dst=dest_rank)
+            elif dest_rank == rank:
+                params = torch.zeros(2, )
+                print(params)
+                params = dist.recv(tensor=params, src=0)
+                print(f"I am rank {rank} and have params {params}")
+
+
+    def _setup_rank(self, rank, size):
+        # initialize the process group
+        dist.init_process_group("gloo", rank=rank, world_size=size)
+
+    def run_rank(self, rank, size):
+        self._setup_rank(rank, size)
         max_iter = self.training_configuration["n_steps"]
-
-        if mpi_rank == 0:
+        loss_fn = self._get_loss()
+        n_samples = 10
+        if rank == 0:
+            self._make_dirs()
             self._setup_flow()
-            optimizer = self._get_optimizer_and_scheduler()
-            #optimizer, scheduler = self._get_optimizer_and_scheduler()
-
-            self.posteriors_path = self.results_path / "posteriors"
-            self.fits_path = self.results_path / "fits"
-
-            if self.posteriors_path.exists():
-                shutil.rmtree(self.posteriors_path)
-            self.posteriors_path.mkdir(parents=True)
-            if self.fits_path.exists():
-                shutil.rmtree(self.fits_path)
-            self.fits_path.mkdir(parents=True)
-
+            optimizer, scheduler = self._get_optimizer_and_scheduler()
             best_loss = np.inf
             loss_hist = []
 
         for it in tqdm(range(max_iter)):
-            if mpi_rank == 0:
+            if rank == 0:
                 optimizer.zero_grad()
 
-            # Sample parameters
-            score = self._get_forecast_score(
-                loss_fn=loss_fn,
-                n_samples=self.training_configuration["n_samples"],
-            )
-            # self._plot_prediction(cases_mean, cases_std, self.data_observable["cases_per_timestep"], it + 1)
-            # plt.close()
-            # score.backward()
-            if mpi_rank == 0:
+            score = self.compute_score(rank, size, loss_fn, n_samples)
+            if rank == 0:
                 optimizer.step()
                 loss_hist.append(score.item())
                 if score.item() < best_loss:
                     torch.save(self.nfm.state_dict(), "./best_model.pth")
                     best_loss = score.item()
-                self._plot_posterior(it + 1)
-                plt.close()
-                self._plot_loss_hist(loss_hist)
-                plt.close()
 
-            # Log loss
-        loss_hist = np.array(loss_hist)
-        return
+    def run(self):
+        size = 2
+        processes = []
+        for rank in range(size):
+            p = mp.Process(target=self.run_rank, args=(rank, size))
+            p.start()
+            processes.append(p)
+        for p in processes:
+            p.join()
--- scripts/run_normflows.py
+++ scripts/run_normflows.py
@@ -1,6 +1,13 @@
-import sys
-
-from bayesian_gradabm.normflows import NormFlows
-
-nf = NormFlows.from_file(sys.argv[1])
-nf.run()
+if __name__ == "__main__":
+    import sys
+    import torch.multiprocessing as mp
+    try:
+       mp.set_start_method('spawn', force=True)
+       print("spawned")
+    except RuntimeError:
+       pass
+    
+    from bayesian_gradabm.normflows import NormFlows
+    
+    nf = NormFlows.from_file(sys.argv[1])
+    nf.run()
